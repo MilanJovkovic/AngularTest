9d17b1c83ae37f259470a550436a8d39a47efa09
Spingkee <pingkee317@gmail.com>
2020-02-21 20:09:41 +0100

    final test result

--- src/app/effects/todo.effect.ts
+++ src/app/effects/todo.effect.ts
@@ -1,6 +1,6 @@
 import { Injectable } from "@angular/core";
 import { Actions, Effect, ofType } from "@ngrx/effects";
-import { delay, mergeMap, takeUntil } from "rxjs/operators";
+import { delay, mergeMap, takeUntil, concatMap, map } from "rxjs/operators";
 import { of } from "rxjs";
 
 import { TodoActionTypes } from "../shared/enum/todo-action-types.enum";
@@ -15,7 +15,15 @@ export class TodoEffect {
   processAction = this.action.pipe(
     mergeMap(payload => {
       console.log(payload);
+      if (payload.type == TodoActionTypes.Stop) {
+        //restart changes
+        return of(payload).pipe(
+          delay(1000), //action delay
+          takeUntil(this.action.pipe(ofType(TodoActionTypes.Increase)))
+        );
+      }
       return of(payload).pipe(
+        //stop action
         delay(1000), //action delay
         takeUntil(this.action.pipe(ofType(TodoActionTypes.Stop)))
       );
